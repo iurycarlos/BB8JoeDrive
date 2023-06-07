9f8f1f1283820e0c3aceb78dca384bd4edd1bb68
Iury Carlos <iurycs@gmail.com>
2023-06-06 22:23:18 -0300

    chore: june version - crashing

--- mega/MK3_Drive.ino
+++ mega/MK3_Drive.ino
@@ -1,10 +1,10 @@
   // ===================================================================================================================================================================================================== 
-  //                         Joe's Drive  - Main drive mechanism - Updated 5/2/18
+  //                         - Main drive mechanism - Updated 5/2/18
   //
   //             ***         You are free to use, and share this code as long as it is not sold. There is no warranty, guarantee, or other tomfoolery. 
-  //                         This entire project was masterminded by an average Joe, your mileage may vary. 
+  //                          
   // ===================================================================================================================================================================================================== 
-  //                            Written by Joe Latiola - https://www.facebook.com/groups/JoesDrive/
+  //                            
   //
   //                            You will need libraries: EepromEX: https://github.com/thijse/Arduino-EEPROMEx
   //                                                     PIDLibrary: http://playground.arduino.cc/Code/PIDLibrary  
@@ -18,12 +18,12 @@
   // Update these as necessary to match your setup
   
   //*****************These only matter if you're NOT using Serial3 for sounds
-  #define soundpin1 26          // Connected to sound pin 0
-  #define soundpin2 28          // Connected to sound pin 1
-  #define soundpin3 30          // Connected to sound pin 2
-  #define soundpin4 32          // Connected to sound pin 3
-  #define soundpin5 46          // Connected to sound pin 4
-  #define soundpin6 44          // Connected to sound pin 5
+  #define soundpin1 22          // Connected to sound pin 0 
+  #define soundpin2 23          // Connected to sound pin 3 
+  #define soundpin3 24          // Connected to sound pin 1 
+  #define soundpin4 25          // Connected to sound pin 4 
+  #define soundpin5 26          // Connected to sound pin 2 
+  #define soundpin6 27          // Connected to sound pin 5 
   //#################################################################
 
   //*****************These only matter if you're using Serial3 for sounds
@@ -41,7 +41,7 @@
   #define quickMusic3     35
   #define quickMusic4     36
   #define quickMusic5     38
-  #define SFX_RST 37
+  #define SFX_RST 35
   //#################################################################
   
   //************************ Dome Tilt MK2 Settings: *****************
@@ -61,29 +61,28 @@
   #define MaxDomeTiltX  12      // Maximum angle to tilt the dome in the X axis ** - MAX IS 18
   #define DomeYEase .4          // Spead of front to back dome movement, higher == faster
   #define DomeXEase .7          // Speed of side to side domemovement, higher == faster
-  #define domeSpeed 60          // Speed that the servos will move
+  #define domeSpeed 70          // Speed that the servos will move
   
 
   //#################################################################
 
-  
   #define domeServoModeAngle 85 // Angle in which dome will look left/right when in servo mode 
-  #define S2SenablePin 29       // Pin that provides power to motor driver enable pins
-  #define enablePin 31          // Pin that provides power to motor driver enable pins
-  #define enablePinDome 33      // Pin that provides power to Dome motor driver enable pin
+  #define S2SenablePin 33       // Pin that provides power to motor driver enable pins
+  #define enablePin 29          // Pin that provides power to motor driver enable pins
+  #define enablePinDome 31      // Pin that provides power to Dome motor driver enable pin
   #define S2SpotPin A0          // Pin connected to side tilt potentiometer 
-  #define ACTpin 34            // Pin connected to ACT on soundboard
-  #define fadePin A2            // Connected to + of one channel on sound board(use resistor to ground)
+  #define ACTpin 37             // Pin connected to ACT on soundboard NAO ESTA CONECTADO NA PCB DO LUKE
+  #define fadePin A8            // Connected to + of one channel on sound board(use resistor to ground)
   #define easeDomeUp 23         // Lower number means more easing when spinning
   #define easeDomeDown 35       // Lower number means more easing when spinning
   #define domeSpinPot A4        // Pin used to monitor dome spin potentiometer
   #define battMonitor A3        // Pin used to monitor battery voltage
-  #define outputVoltage 5.2     // This is the output voltage from the Buck Converter powering the arduino
-  #define drivePWM1 12          // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
-  #define drivePWM2 13          // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
-  #define s2sPWM1 6             // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
-  #define s2sPWM2 7             // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
-  #define maxS2STilt 20         // max tilt using the joystick; max is 25
+  #define outputVoltage 5.0     // This is the output voltage from the Buck Converter powering the arduino
+  #define drivePWM1 13          // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed --- 
+  #define drivePWM2 12          // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed --- 
+  #define s2sPWM1 7            // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed -- era 6 antes
+  #define s2sPWM2 6           // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed -- era 7 antes
+  #define maxS2STilt 55         // max tilt using the joystick; max is 25 , estava 75 no code reg, vou tentar 35 
   #define domeSpinPWM1 10       // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
   #define domeSpinPWM2 11       // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed
   #define flywheelSpinPWM1 8    // PWM Pin for movement, swap the pin numbers on this axis if axis is reversed 
@@ -91,8 +90,8 @@
   #define resistor1 151000      // Larger resisitor used on voltage divider to read battery level
   #define resistor2 82000       // Smaller resisitor used on voltage divider to read battery level
   #define flywheelEase 3        // Speed in which flywheel will increase/decrease during gradual movements
-  #define S2SEase 2.5           // speed in which side to side moves. Higher number equates to faster movement
-  #define resetPin 40
+  #define S2SEase 6.5           // 2.5 default speed in which side to side moves. Higher number equates to faster movement
+  #define resetPin 39
   #define drivespeed1 50        // Speed of "Slow"
   #define drivespeed2 70        // Speed of "Med"
   #define drivespeed3 110       // Speed of "Fast" DO NOT EXCEED 110
@@ -102,35 +101,36 @@
 
 
 //**************************** Choose the options that match your setup ****************************
-  //#define MK2_Dome
+ 
   #define MK3_Dome
 
-  #define SerialSound           // ONLY ENABLE IF YOU HAVE THE SOUNDBOARD WIRED TO SERIAL3
+ //#define SerialSound           // ONLY ENABLE IF YOU HAVE THE SOUNDBOARD WIRED TO SERIAL3
   
   //#define disablePSIflash       // Uncomment to remove the PSI flash.    
   
-  //#define TiltDomeForwardWhenDriving      // uncomment this if you want to tilt the dome forward when driving. 
+  #define TiltDomeForwardWhenDriving      // uncomment this if you want to tilt the dome forward when driving. 
 
 
 
 //**************************** Use these to correct any reversals ****************************
 
   //Controller settings:
-      //#define reverseDrive          // uncomment if your drive joystick up/down is reversed
+      #define reverseDrive          // uncomment if your drive joystick up/down is reversed
       //#define reverseDomeTilt       // uncomment if your dome tilt joystick is reversed
-      //#define reverseS2S            // uncomment if your side to side joystick is reversed
-      #define reverseDomeSpin         // uncomment if your dome spin joystick is reversed
-      #define reverseDomeSpinServo    // uncomment if your dome spin joystick is reversed in servo mode
+    // #define reverseS2S            // uncomment if your side to side joystick is reversed
+      //#define reverseDomeSpin        // uncomment if your dome spin joystick is reversed
+      //#define reverseDomeSpinServo   // uncomment if your dome spin joystick is reversed in servo mode
       //#define reverseFlywheel       // uncomment if your flywheel joystick is reversed
 
   //IMU settings:
-      //#define reversePitch          // reverse Pitch. Test this by moving the drive by hand; the weight/stabilization should move WITH you, NOT AGAINST. This means the weight should always stay down. 
-      //#define reverseRoll           // reverse Roll. Test this by moving the drive by hand; the weight/stabilization should move WITH you, NOT AGAINST. This means the weight should always stay down.
-
+      #define reversePitch          // reverse Pitch. Teste isso movendo o corpo manualmente; o peso/estabilização deve se mover COM você e NÃO CONTRA você. Isso significa que o peso deve sempre permanecer pra baixo.
+      
+      // ~ #define reverseRoll           // reverse Roll. Teste isso movendo o corpo manualmente; o peso/estabilização deve se mover COM você e NÃO CONTRA você. Isso significa que o peso deve sempre permanecer pra baixo.
+      
   //Potentiometer settings:
-      #define reverseDomeTiltPot      // uncomment to reverse this pot. MK2 ONLY This is needed if the dome tilt arm oscilates back/forth
+      //#define reverseDomeTiltPot      // uncomment to reverse this pot. MK2 ONLY This is needed if the dome tilt arm oscilates back/forth
       //#define reverseDomeSpinPot    // uncomment to reverse dome spin pot. If the dome spin acts strange when in servo mode, this is likely the culprit. 
-      #define reverseS2SPot           // uncomment to reverse the side tilt pot. If your side to side is going apesh*t, try this one. 
+        #define reverseS2SPot           // uncomment to reverse the side tilt pot. If your side to side is going apesh*t, try this one. 
 
   //PWM settings:
       // If your drive takes off by itself as soon as you enable it, try reversing the Drive PWM pins. 
@@ -142,19 +142,19 @@
 
 //**************************** Debug  ****************************
   
-           //#define printRemote              // Uncomment to see values passed from controller
-           //#define debugS2S                 // Uncomment to see Side tilt variables, PID values, ETC.
-           //#define debugDrive               // Uncomment to see main drive variables, PID values, ETC.
-           //#define debugDomeTilt            // Uncomment to see Dome tilt variables, PID values, ETC.
-           //#define debugdomeRotation        // Uncomment to see Dome rotation variables, PID values, ETC.
-           //#define debugPSI                 // Uncomment to see PSI values.
-           //#define printbodyBatt            // Uncomment to see battery level 
-           //#define printYPR                 // Uncomment to see Yaw, Pitch, and Roll
-           //#define printDome                // Uncomment to see the Dome's Yaw
-           //#define printOffsets             // Uncomment to see the offsets
-           //#define printOutputs
-           //#define printSoundPins
-           //#define debugFlywheelSpin
+          // #define printRemote              // Uncomment to see values passed from controller
+          // #define debugS2S                 // Uncomment to see Side tilt variables, PID values, ETC.
+          // #define debugDrive               // Uncomment to see main drive variables, PID values, ETC.
+          // #define debugDomeTilt            // Uncomment to see Dome tilt variables, PID values, ETC.
+          // #define debugdomeRotation        // Uncomment to see Dome rotation variables, PID values, ETC.
+          // #define debugPSI                 // Uncomment to see PSI values.
+          // #define printbodyBatt            // Uncomment to see battery level 
+           #define printYPR                 // Uncomment to see Yaw, Pitch, and Roll
+          // #define printDome                // Uncomment to see the Dome's Yaw
+          // #define printOffsets             // Uncomment to see the offsets
+          // #define printOutputs
+          // #define printSoundPins
+          // #define debugFlywheelSpin
   
   
   
@@ -193,7 +193,7 @@
         byte Speed;
         byte rBut2; //right 2
         byte rBut3; //right 3
-        byte motorEnable=1; //toggle on top
+        byte motorEnable=1 ; //toggle on top
         byte CalibID; 
         byte wireless=1;    
   }recFromRemote; 
@@ -315,39 +315,50 @@
   unsigned long lastLoopMillis;
   unsigned long IMUMillis, lastRecMillis, moveDataMillis, lastReceivedMillis;
   unsigned long lastBatteryUpdate;
-  
-  
 
+   
+  // PK : quanto maior, menor o erro em estado permanente, porem um valor muito alto, desestabiliza o sistema todo
+  // PI : quanto maior, mais rápido chega no valor final, porem um valor muito alto, desestabiliza o sistema todo
+  // PD : tem que ser quase zero pois a medida que aumenta, aumenta as oscilações e desestabilização do sistema
+
+  //                      EFEITOS DO AUMENTO DE CADA PARAMETRO PID :
+  // PARAMETRO | TEMPO ATÉ O ALVO | ULTRAPASSAGEM DO ALVO | TEMPO DE ACOMODAÇÃO | ERRO ESTADO ESTACIONARIO |    ESTABILIDADE
+  //    PK            DIMINUI              AUMENTA              POUCO MUDA                DIMINUI                PIORA
+  //    PI            DIMINUI              AUMENTA                AUMENTA                 ELIMINA                PIORA
+  //    PD          POUCO MUDA             DIMINUI                DIMINUI                SEM EFEITO        MELHORA SE PK PEQUENO
   
-  
-  //PID1 is for the side to side tilt
-  double Pk1 = 14;  
-  double Ik1 = 0;
-  double Dk1 = 0.0;
+  //PID1 is for the side to side tilt - inclinação lateral
+
+  double Pk1 = 5; // valor original : 14 // ultimo valor 14
+  double Ik1 = 0; // era 0
+  double Dk1 = 0; //  era 0
   double Setpoint1, Input1, Output1, Output1a;    
   
   PID PID1(&Input1, &Output1, &Setpoint1, Pk1, Ik1 , Dk1, DIRECT);   
    
-  //PID2 is for side to side stability
-  double Pk2 = .5; 
-  double Ik2 = 0;
-  double Dk2 = .01;
+  //PID2 is for side to side stability - estabilidade lateral
+  
+  double Pk2 = 0.3; // valor original : 0.5 // ultimo valor : 0.5
+  double Ik2 = 0; // era 0
+  double Dk2 = .005;   // original  0.01
   double Setpoint2, Input2, Output2, Output2a;    
   
   PID PID2(&Input2, &Output2, &Setpoint2, Pk2, Ik2 , Dk2, DIRECT);    // PID Setup - S2S stability   
   
   //PID3 is for the main drive
-  double Pk3 = 5; 
+  
+  double Pk3 = 3; // valor original : 5 // ultimo valor : 7
   double Ik3 = 0;
-  double Dk3 = 0;
+  double Dk3 = 0; // valor original : 0 // ultimo valor : 0.03
   double Setpoint3, Input3, Output3, Output3a;    
   
   PID PID3(&Input3, &Output3, &Setpoint3, Pk3, Ik3 , Dk3, DIRECT);    // Main drive motor
   
   //PID4 is for dome tilt fwd/back
-  double Pk4 = 6;  
+  
+  double Pk4 = 6 ; // valor original 6 // ultimo valor 10  
   double Ik4 = 0;
-  double Dk4 = 0.05;
+  double Dk4 = 0.06; // valor original 0.06 // ultimo valor 0.07                                                                                  
   double Setpoint4, Input4, Output4, Output4a;    
   
   PID PID4(&Input4, &Output4, &Setpoint4, Pk4, Ik4 , Dk4, DIRECT);   
@@ -355,16 +366,13 @@
   double Setpoint5a;
   
   //PID5 is for the dome spin servo
+  
   double Kp5=4, Ki5=0, Kd5=0;
   double Setpoint5, Input5, Output5, Output5a;
   
   PID PID5(&Input5, &Output5, &Setpoint5, Kp5, Ki5, Kd5, DIRECT);     
   
 
-  
-
-  
-  
   // ================================================================
   // ===                      INITIAL SETUP                       ===
   // ================================================================
@@ -380,7 +388,7 @@
       #ifdef MK3_Dome
       leftServo.attach(leftDomeTiltServo);
       rightServo.attach(rightDomeTiltServo);
-      leftServo.write(95,50, false); 
+      leftServo.write(90, 50, false); 
       rightServo.write(90, 50, false);
       #endif
       
@@ -392,7 +400,7 @@
       pinMode(S2SenablePin, OUTPUT); //enable pin for S2S
       pinMode(enablePinDome, OUTPUT);  // enable pin for dome spin
       pinMode(13, OUTPUT);
-      digitalWrite(13, LOW);
+      digitalWrite(13, HIGH);
       pinMode(ACTpin, INPUT_PULLUP); // read stat of Act on Soundboard
       
 
@@ -494,5 +502,3 @@
      sendDriveData();
   }
   
-
-
